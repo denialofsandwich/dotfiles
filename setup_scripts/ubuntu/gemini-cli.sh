213a0a2094385f176334b268d4366be04e4972c7
Rene Fa <farene1996@gmail.com>
2026-02-06 13:13:12 +0100

    fix(gemini): Use nvm so installation does not require root anymore

--- setup_scripts/ubuntu/gemini-cli.sh
+++ setup_scripts/ubuntu/gemini-cli.sh
@@ -5,8 +5,18 @@ pushd $(dirname $0)
 
 echo -e "\033[33m### SETUP $name\033[0m"
 
-sudo apt-get install -y npm
-sudo npm install -g @google/gemini-cli
+export NVM_DIR="$HOME/.nvm"
+export PROFILE=/dev/null
+mkdir $NVM_DIR
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+nvm alias default node
+nvm use default
+
+npm install -g @google/gemini-cli
 
 echo "Update stow"
 popd
