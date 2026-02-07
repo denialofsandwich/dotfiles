7f7a9351c74d3cce44395e1c510b0b8ebe89c265
René Fa <farene1996+github@gmail.com>
2026-02-07 02:04:35 +0100

    docs(neovim): Add comment how to load a local plugin

--- stow_packages/neovim/nvim/lua/plugins/sudo.lua
+++ stow_packages/neovim/nvim/lua/plugins/sudo.lua
@@ -1,6 +1,8 @@
 return {
   {
     "denialofsandwich/sudo.nvim",
+    cmd = { "SudoRead", "SudoWrite", "SudoEdit" },
+    -- dir = "/home/username/dev/sudo.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
     },
