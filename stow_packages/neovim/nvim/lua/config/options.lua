59e91f7607092467fb0d1e1d8d13d7d40f67e38f
Rene Fa <farene1996@gmail.com>
2026-03-26 13:00:00 +0100

    refactor(neovim): Remove redundant options

--- stow_packages/neovim/nvim/lua/config/options.lua
+++ stow_packages/neovim/nvim/lua/config/options.lua
@@ -2,18 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- LSP Server to use for Python.
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff"
-
 vim.g.python3_host_prog = "~/.local/nvim/venv/bin/python"
 vim.g.python_host_prog = "~/.local/nvim/venv/bin/python"
 
-vim.opt.relativenumber = false
 vim.opt.shell = "/bin/zsh"
 vim.opt.wrap = true
 vim.opt.spell = true
-vim.opt.clipboard = ""
-vim.opt.formatoptions:remove({ "r", "o" })
-vim.opt.conceallevel = 0
 vim.g.root_spec = { "cwd" }
