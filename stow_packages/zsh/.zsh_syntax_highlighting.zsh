6b4af5a929be93e56fa5670cf029c7cbcc22abd3
Rene Fa <farene1996@gmail.com>
2026-04-23 15:31:29 +0200

    feat(zsh): Make zsh_syntax_highlighting theme-agnostic

--- stow_packages/zsh/.zsh_syntax_highlighting.zsh
+++ stow_packages/zsh/.zsh_syntax_highlighting.zsh
@@ -1,67 +1,64 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 typeset -gA ZSH_HIGHLIGHT_STYLES
-## General
-### Diffs
-### Markup
-## Classes
+
 ## Comments
-ZSH_HIGHLIGHT_STYLES[comment]='fg=#7F8C8D'
-## Constants
-## Entitites
+ZSH_HIGHLIGHT_STYLES[comment]='fg=8'
+
 ## Functions/methods
-ZSH_HIGHLIGHT_STYLES[alias]='fg=#33DD2D'
-ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=#33DD2D'
-ZSH_HIGHLIGHT_STYLES[global-alias]='fg=#33DD2D'
-ZSH_HIGHLIGHT_STYLES[function]='fg=#33DD2D'
-ZSH_HIGHLIGHT_STYLES[command]='fg=#33DD2D'
-ZSH_HIGHLIGHT_STYLES[precommand]='fg=#33DD2D,italic'
-ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=#FFBB00,italic'
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=#FF8550'
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=#FF8550'
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=#FF8550'
-## Keywords
+ZSH_HIGHLIGHT_STYLES[alias]='fg=10'
+ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=10'
+ZSH_HIGHLIGHT_STYLES[global-alias]='fg=10'
+ZSH_HIGHLIGHT_STYLES[function]='fg=10'
+ZSH_HIGHLIGHT_STYLES[command]='fg=10'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=10,italic'
+
+ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=11,italic'
+
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=9'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=9'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=9'
+
 ## Built ins
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=#17E6D5'
-ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=#17E6D5'
-ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=#17E6D5'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=14'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=14'
+ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=14'
+
 ## Punctuation
-ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=#AC1C44'
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=#9058D5'
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]='fg=#8027D1'
-ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=#9058D5'
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=#AC1C44'
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=#AC1C44'
-ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]='fg=#AC1C44'
-## Serializable / Configuration Languages
-## Storage
+ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=1'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=13'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]='fg=5'
+ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=13'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=1'
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=1'
+ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]='fg=1'
+
 ## Strings
-ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]='fg=#FFBB00'
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]='fg=#FFBB00'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=#FFBB00'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=#E61717'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=#FFBB00'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=#E61717'
-ZSH_HIGHLIGHT_STYLES[rc-quote]='fg=#FFBB00'
+ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]='fg=11'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]='fg=11'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=11'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=1,bold'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=11'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=1,bold'
+ZSH_HIGHLIGHT_STYLES[rc-quote]='fg=11'
+
 ## Variables
-ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=#8027D1'
-ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]='fg=#E61717'
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=#8027D1'
-ZSH_HIGHLIGHT_STYLES[assign]='fg=#FCFCFC'
-ZSH_HIGHLIGHT_STYLES[named-fd]='fg=#FCFCFC'
-ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=#FCFCFC'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=5'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]='fg=1,bold'
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=5'
+ZSH_HIGHLIGHT_STYLES[assign]='fg=default'
+ZSH_HIGHLIGHT_STYLES[named-fd]='fg=default'
+ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=default'
+
 ## No category relevant in spec
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#E61717'
-ZSH_HIGHLIGHT_STYLES[path]='fg=#FCFCFC'
-ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=#FF7200'
-ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=#AAAAAA'
-ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]='fg=#AC501C'
-ZSH_HIGHLIGHT_STYLES[globbing]='fg=#9058D5'
-ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=#8027D1'
-#ZSH_HIGHLIGHT_STYLES[command-substitution]='fg=?'
-#ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]='fg=?'
-#ZSH_HIGHLIGHT_STYLES[process-substitution]='fg=?'
-#ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]='fg=?'
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]='fg=#E61717'
-ZSH_HIGHLIGHT_STYLES[redirection]='fg=#FCFCFC'
-ZSH_HIGHLIGHT_STYLES[arg0]='fg=#FCFCFC'
-ZSH_HIGHLIGHT_STYLES[default]='fg=#FCFCFC'
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=1,bold'
+ZSH_HIGHLIGHT_STYLES[path]='fg=default'
+ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=3'
+ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=7'
+ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]='fg=3'
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=13'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=5'
+
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]='fg=1,bold'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=default'
+ZSH_HIGHLIGHT_STYLES[arg0]='fg=default'
+ZSH_HIGHLIGHT_STYLES[default]='fg=default'
