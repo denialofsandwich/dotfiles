81500af0e98eadbde70d645548ea59878199a439
Rene Fa <farene1996@gmail.com>
2026-08-06 12:02:39 +0200

    feat(neovim): Switch to ty

--- stow_packages/neovim/nvim/lua/plugins/lspconfig.lua
+++ stow_packages/neovim/nvim/lua/plugins/lspconfig.lua
@@ -1,12 +1,31 @@
 return {
   {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "ty")
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         pyright = {
+          enabled = false,
+          -- Skipped if disabled
           on_attach = function(client, _)
             client.server_capabilities.codeActionProvider = false
           end,
+        },
+        ty = {
+          settings = {
+            ty = {
+              inlayHints = {
+                variableTypes = false,
+                callArgumentNames = false,
+              },
+            },
+          },
         },
       },
     },
