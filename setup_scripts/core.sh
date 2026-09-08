bc84c5fd317bbe9ff87d340b149de5e3cabbb8b7
Rene Fa <farene1996@gmail.com>
2026-09-08 13:54:23 +0200

    refactor(core): Update docs and linting warnings

--- setup_scripts/core.sh
+++ setup_scripts/core.sh
@@ -4,25 +4,31 @@
 if [[ "$MODE" == "install" ]] && ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-elif [[ $OS_TYPE == "linux" && "$MODE" == "uninstall" ]]; then
+elif [[ "$MODE" == "uninstall" && "$OS_TYPE" == "linux" ]]; then
+  # Uninstall works slightly differently and requires root on macos
   brew list | xargs brew uninstall -y
   rm -rf /home/linuxbrew
 fi
 
-if [[ $ROOT_PACKAGES == "yes" && $OS_TYPE == "linux" ]]; then
-  sudo $LINUX_PKG_MGR install -y \
+# If some packages need to be globally available on linux
+if [[ "$ROOT_PACKAGES" == "yes" && "$OS_TYPE" == "linux" ]]; then
+  sudo "$LINUX_PKG_MGR" install -y \
     htop lsof curl jq git vim screen tmux bash zsh croc
 fi
 
 brew "$MODE" -y htop lsof curl jq stow zip git croc lazysql yq jinja2-cli fastfetch
 
-if [[ $OS_TYPE == "linux" ]]; then
+# OS specific packages
+if [[ "$OS_TYPE" == "linux" ]]; then
   brew "$MODE" -y dysk
-elif [[ $OS_TYPE == "macos" ]]; then
+elif [[ "$OS_TYPE" == "macos" ]]; then
   brew "$MODE" -y telnet coreutils
 fi
 
-if [[ $OS == "steamos" ]]; then
+# Steam OS is shipping without build essentials and because it's an immutable OS
+# It can't be installed system-wide. This workaround installs them user-wide
+# to allow building packages from source.
+if [[ "$OS" == "steamos" ]]; then
   brew "$MODE" -y glibc gcc
   mkdir -p ~/.env_scripts
   cat >~/.env_scripts/gcc_fix.sh <<EOF
