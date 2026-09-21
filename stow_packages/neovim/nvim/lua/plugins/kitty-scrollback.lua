efc714070113bd9955458190a921cd4cc588b5c0
Rene Fa <farene1996@gmail.com>
2026-09-21 13:17:38 +0200

    feat(neovim): Update kitty-scrollback.nvim and use forked version until merged

--- stow_packages/neovim/nvim/lua/plugins/kitty-scrollback.lua
+++ stow_packages/neovim/nvim/lua/plugins/kitty-scrollback.lua
@@ -1,10 +1,12 @@
 return {
-  "mikesmithgh/kitty-scrollback.nvim",
+  "denialofsandwich/kitty-scrollback.nvim",
   enabled = true,
   lazy = true,
   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth", "KittyScrollbackGenerateCommandLineEditing" },
   event = { "User KittyScrollbackLaunch" },
-  config = function()
-    require("kitty-scrollback").setup()
-  end,
+  opts = {
+    {
+      close_after_yank = false,
+    },
+  },
 }
