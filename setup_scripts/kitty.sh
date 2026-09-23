1a6290dfa84f97ffa79457a4044b2bbb649911fd
Rene Fa <farene1996@gmail.com>
2026-09-23 12:33:20 +0200

    feat(kitty): Use cask on linux

--- setup_scripts/kitty.sh
+++ setup_scripts/kitty.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 # Setup kitty, a fast gpu-based terminal emulator
 
-if [[ $OS_TYPE == "linux" ]]; then
-  if [[ $MODE == "install" ]]; then
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
-    ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-    cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-    cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-    sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-    sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-  else
-    rm -rf ~/.local/kitty.app
-    rm ~/.local/bin/kitty ~/.local/bin/kitten
-    rm ~/.local/share/applications/kitty*.desktop
-  fi
-else
-  brew "$MODE" -y kitty
-fi
+brew "$MODE" -y kitty
 
-mkdir -p ~/.kitty-sessions
+if [[ $OS_TYPE == "linux" ]]; then
+  # The brew cask installs the kitty binary and app data, but doesn't register
+  # a desktop file, so we need to do that ourselves.
+  if [[ $MODE == "install" ]]; then
+    kitty_prefix="$(dirname "$(dirname "$(readlink -f "$(brew --prefix)/bin/kitty")")")"
+    cp "$kitty_prefix/share/applications/kitty.desktop" ~/.local/share/applications/
+    cp "$kitty_prefix/share/applications/kitty-open.desktop" ~/.local/share/applications/
+    sed -i "s|Icon=kitty|Icon=$kitty_prefix/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+    sed -i "s|Exec=kitty|Exec=$kitty_prefix/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+  else
+    rm -f ~/.local/share/applications/kitty*.desktop
+  fi
+
+  mkdir -p ~/.kitty-sessions
+fi
 
 stow_update_templated ~/.config/kitty
